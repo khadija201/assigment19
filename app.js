f93b7b05fbb299a3fa5332a5115c41c8edcf878c
khadija201 <khadija2qubra@gmail.com>
2024-03-03 22:32:08 +0500

    assigment 19 done

---
+++ app.js
@@ -0,0 +1,78 @@
+// Array 1
+var alphabet = ["h" , "i","j","k","l","m"]    
+alert(alphabet [2]);
+// #
+ var alphabet = ["h" , "i","j","k","l","m"]
+var Arraylength = alphabet.length ;
+console.log (Arraylength);
+// #
+var arryElement = ["firstElement"];
+myArray= "secondElement";
+alert(myArray)
+// 2
+var alphabet = ["h" , "i","j","k","l","m"]    
+alert (alphabet.push("y"))
+// #
+var alphabet = ["h" , "i","j","k","l","m"]    
+alert (alphabet.pop("m"))
+// #
+var alphabet=["h","i","j","k"]
+ alphabet.unshift()
+console.log(alphabet)
+// 3
+var pet=["lion","cat","fish","frog","duck","dog"]
+var reg=pet.slice(2,3,"ox","tiger")
+console.log(reg)
+// #
+var pet=["lion","cat","frog","duck","dog"]
+var regPet=pet.slice(2,3);
+console.log(regPet)
+// #
+var pet=["lion","cat","frog","duck","dog","flise","fish","ox"]
+var pets= pet.slice (3,5)
+console.log(pets)
+// 4
+var size =["S","M","XL","XXL","XXXL"] 
+size.pop();
+console.log(size)
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+size.unshift(3,2,1)
+console.log(size);
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+size.splice(3,0,"L")
+console.log(size);
+// #
+var size =["S","M","XL","XXL","XXXL"] 
+var regsize=size.slice(0,"L")
+console.log(regsize);
+
+// 5
+var qulification=["BCOM","BS","BSS","ADA","SSC","COMP","MS"]
+console.log(qulification)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
